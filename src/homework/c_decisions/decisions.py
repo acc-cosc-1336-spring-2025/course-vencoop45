aeb80934ed4cdb193134aeaade5d4858b979f58a
vencoop45 <ven.cooper@g.austincc.edu>
2025-03-09 01:39:58 +0000

    homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,16 @@
 #
+def get_options_ratio(options, total):
+    ratio = options / total
+    return ratio
+
+def get_faculty_rating(ratio):
+    if (ratio <= 1 and ratio >= .9):
+        return "Excellent"
+    elif (ratio < .9 and ratio >= .8):
+        return "Very Good"
+    elif (ratio < .8 and ratio >= .7):
+        return "Good"
+    elif (ratio < .7 and ratio >= .6):
+        return "Needs Improvement"
+    elif (ratio < .6 and ratio >= 0):
+        return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,11 @@
 #
+import decisions
+
+options = int(input("Enter here the amount of options:"))
+
+total = int(input("Enter here the total:"))
+
+result = decisions.get_options_ratio(options, total)
+
+faculty_rating = decisions.get_faculty_rating(result)
+print("Faculty Rating", faculty_rating)
