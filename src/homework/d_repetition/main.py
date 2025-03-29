18e76c4bc81a361d7d149585c0b0f4b9642cc2e7
vencoop45 <ven.cooper@g.austincc.edu>
2025-03-29 06:22:36 +0000

    homework 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,40 @@
-#
+import repetition
+
+def main():
+    while True:
+        print("\nHomework 3 Menu:")
+        print("1 - Calculate Factorial")
+        print("2 - Sum Odd Numbers")
+        print("3 - Exit")
+
+        choice = input("Please enter choice (1-3):")
+
+        if choice == '1':
+            while True:
+                try:
+                    num_str = input("Please enter a number greater than 0 and less than 10:")
+                    num = int(num_str)
+                    if 0 < num < 10:
+                        factorial_result = repetition.get_factorial(num)
+                        print(f"The factorial of {num} is: {factorial_result}")
+                        break
+                    print("Invalid input. Please try again.")
+
+        elif choice == '2':
+            while True:
+                try:
+                    num_str = input("Please enter a number greater than 0 and less than 100:")
+                    num = int(num_str)
+                    if 0 < num < 100:
+                        sum_odd = repetition.sum_odd_numbers(num)
+                        print(f"The sum of odd numbers up to {num} is: {sum_odd}")
+                        break
+                    print("Invalid input. Please try again.")
+        
+        elif choice == '3':
+            verify = input("Please verify you would like to exit. (yes/no):").lower()
+            if verify == 'yes':
+                break
+
+if __name__ == "__main__":
+    main()
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,15 @@
 #
+def get_factorial(num):
+    factorial = 1
+    for i in range(1 , num + 1):
+        factorial *= i
+    return factorial
+
+def sum_odd_numbers(num):
+    sum = 0
+    i = 1
+    while i <= num:
+        if i % 2 != 0:
+            sum += i
+        i += 2
+    return sum
