c9864961ca7d98424cbd1acfa762d42ef4cf58a9
vencoop45 <ven.cooper@g.austincc.edu>
2025-04-18 05:57:06 +0000

    Homework 7

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,35 @@
-#
+import dictionary
+
+def main():
+    inventory_dictionary = {}
+
+    while True:
+        print("\nInventory Menu")
+        print("1 - Add or Update Item")
+        print("2 - Delete Item")
+        print("3 - Exit")
+
+        choice = input("Please Enter Selection:")
+
+        if choice == '1':
+            while True:
+                widget_name = input("Please Enter Widget Name:")
+                quantity = int(input("Please Enter Quantity:"))
+                dictionary.add_inventory(widgets, widget_name, quantity)
+                print(f"Updated Inventory: {inventory_dictionary}")
+
+        elif choice == '2':
+            while True:
+                widget_name = input("Please Enter Name of Widget to Delete:")
+                result = dictionary.remove_inventory_widget(widgets, widget_name)
+                print(result)
+        
+        elif choice == '3':
+            print("Program Closing")
+            break
+
+        else:
+            print("Invalid Selection. Choose a Number 1-3.")
+
+if __name__ == "__main__":
+    main()
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,24 @@
-#
+import unittest
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
+
+class Test_Config(unittest.TestCase):
+
+    def test_add_inventory(self):
+        inventory_dictionary = {}
+
+        add_inventory(inventory_dictionary, "Widget1", 10)
+        self.assertEqual(inventory_dictionary["Widget1"], 10)
+
+        add_inventory(inventory_dictionary, "Widget1", 25)
+        self.assertEqual(inventory_dictionary["Widget1"], 35)
+
+        add_inventory(inventory_dictionary, "Widget1", -10)
+        self.assertEqual(inventory_dictionary["Widget1"], 25)
+
+    def test_remove_inventory_widget(self):
+        inventory_dictionary = {"Widget1": 50, "Widget2": 20}
+        result = remove_inventory_widget(inventory_dictionary, "Widget1")
+
+        self.assertEqual(result, "Record deleted")
+        self.assertNotIn("Widget1", inventory_dictionary)
+        self.assertIn("Widget2", inventory_dictionary)
